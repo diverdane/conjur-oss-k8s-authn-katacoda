3422a64e88625a34a77ad2ed5cc569f35ddb62a6
Dane LeBlanc <dane.leblanc@cyberark.com>
2020-02-18 22:22:46 -0500

    Fix helm init for kube version >= 1.16

--- 1_install_conjur.sh
+++ 1_install_conjur.sh
@@ -11,11 +11,13 @@ then
 
 fi
 
-helm init
+##initializing helm (see https://github.com/helm/helm/issues/6374)
+helm init --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
+
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --upgrade
 
+helm init --client-only
 helm repo add cyberark https://cyberark.github.io/helm-charts
 helm repo update
 
